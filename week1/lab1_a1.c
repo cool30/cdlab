296eb1bb4af8dd425262802e21309599770a1b0a
cool30 <rk9735921@gmail.com>
2025-01-08 18:56:37 +0530

    Code updated on lab1

--- week1/lab1_a1.c
+++ week1/lab1_a1.c
@@ -2,7 +2,7 @@
 
 void getDetails(char *filename)
 {
-    int blankSpace = 0, semiColon=0, blankLine=0;
+    int blankSpace = 0, semiColon = 0, blankLine = 0;
     FILE *file1 = fopen(filename, "r");
     if (file1 == NULL)
     {
@@ -18,15 +18,24 @@ void getDetails(char *filename)
         }
         if (c1 == ';')
         {
-            semiColon++;
+            c1 = fgetc(file1);
+            if (c1 == '\n')
+            {
+                semiColon++;
+            }
+            else
+            {
+                printf("\nThere is error in the file, Unexpected ; symbol found");
+                return;
+            }
         }
         if (c1 == '\n')
         {
             blankLine++;
         }
-        c1=fgetc(file1);
+        c1 = fgetc(file1);
     }
-    printf("\nBlankSpace: %d", blankSpace);
+    printf("\nBlankSpace(assuming single space and tab space as 1 unit): %d", blankSpace);
     printf("\nSemincolon: %d", semiColon);
     printf("\nBlankLine: %d", blankLine);
     fclose(file1);
